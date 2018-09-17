0aa780fd0ac32cdfa90a782136d7518dc3fdf9b4
Ada Wei <sujin19840419@sina.com>
2018-09-17 17:40:26 +0800

    Add new file

---
+++ 3.py
@@ -0,0 +1,12 @@
+# 将两个列表合并并去掉重复数字升序输出
+list1 = [2, 3, 8, 4, 9, 5, 6]
+list2 = [5, 6, 10, 17, 11, 2]
+list3 = list1 + list2
+print (set(list3))
+
+list = []
+for x in list3:
+    if x not in list:
+        list.append(x)
+list.sort()
+print (list)
